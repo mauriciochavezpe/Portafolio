bb5745534ca2c884bf81d316341bbd5f479a2d4f
mchavez-pe <mauchp0311@gmail.com>
2020-10-19 23:01:03 -0500

    deploy

---
+++ precache-manifest.e40664493a7c646fb6d4c275b03c3aba.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff963112d7cf27aaaa1",
+    "url": "Portafolio/css/about.7984a488.css"
+  },
+  {
+    "revision": "1700f65d9bbaa4071399",
+    "url": "Portafolio/css/app.69ea161d.css"
+  },
+  {
+    "revision": "2703b4b15a7587662f5b726fa120fc0b",
+    "url": "Portafolio/img/PERUCAMBIO.2703b4b1.png"
+  },
+  {
+    "revision": "890296e2b95493be508856fe96bd9c1d",
+    "url": "Portafolio/img/img--bg-contact.890296e2.svg"
+  },
+  {
+    "revision": "ec809063bf7a23adffdb001b7c95528f",
+    "url": "Portafolio/img/sistemasuni.ec809063.png"
+  },
+  {
+    "revision": "949a84036814fb754ba960517b8dc9a3",
+    "url": "Portafolio/index.html"
+  },
+  {
+    "revision": "bff963112d7cf27aaaa1",
+    "url": "Portafolio/js/about.5359b44c.js"
+  },
+  {
+    "revision": "1700f65d9bbaa4071399",
+    "url": "Portafolio/js/app.7c239116.js"
+  },
+  {
+    "revision": "f0a723b3258a58099ae9",
+    "url": "Portafolio/js/chunk-vendors.45f850c2.js"
+  },
+  {
+    "revision": "9120b02fe0586546ad07dc1c6b8517c7",
+    "url": "Portafolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "Portafolio/robots.txt"
+  }
+]);
